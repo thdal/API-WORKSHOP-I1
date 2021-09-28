a7712f0b47db1663dc7a88864f793195fd627a96
Jason Morton <91466288+Little-Marlin@users.noreply.github.com>
2021-09-28 15:56:30 +0200

    rectification erreur SQL

--- sql/PTE_SQL_DDL.sql
+++ sql/PTE_SQL_DDL.sql
@@ -1,20 +1,3 @@
-DROP TABLE IF EXISTS questions;
-DROP TABLE IF EXISTS reponses;
-
-
-SET FOREIGN_KEY_CHECKS = 1;
-
-CREATE TABLE IF NOT EXISTS `questions` (
-  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  bot_question varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
-
-CREATE TABLE IF NOT EXISTS `reponses` (
-  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  bot_reponse varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
-
-
 INSERT INTO questions(bot_question)
 VALUE('Bonjour, comment puis-je vous aider?');
 
