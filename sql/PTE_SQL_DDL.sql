15ec5898362c908e280a720dfe547529a55c19c9
Jason Morton <91466288+Little-Marlin@users.noreply.github.com>
2021-09-28 14:19:16 +0200

    Update PTE_SQL_DDL.sql
    Mise à jour requête BDD

--- sql/PTE_SQL_DDL.sql
+++ sql/PTE_SQL_DDL.sql
@@ -1,33 +1,81 @@
-INSERT INTO questions(bot_question)
-VALUE('bonjour');
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS reponses;
 
-INSERT INTO reponses(bot_reponse)
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+CREATE TABLE IF NOT EXISTS `questions` (
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  bot_question varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+CREATE TABLE IF NOT EXISTS `reponses` (
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  bot_reponse varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+
+INSERT INTO questions(bot_question)
 VALUE('Bonjour, comment puis-je vous aider?');
 
 -- Partie sensibilisation
-INSERT INTO questions(bot_question)
-VALUE('j’ai une question sur la cybersécurité');
+INSERT INTO reponses(bot_reponse)
+VALUE('j’ai une question sur la cybersécurité.');
 
 INSERT INTO questions(bot_question)
-VALUE('Voici les sujets que je peux proposer');
+VALUE('Voici les sujets que je peux proposer.');
 
-INSERT INTO questions(bot_question)
+INSERT INTO reponses(bot_reponse)
+VALUE('L’escroquerie aux faux ordres de virement (FOVI).');
+
+INSERT INTO reponses(bot_reponse)
+VALUE('Hameçonnage (phishing en anglais).');
+
+INSERT INTO reponses(bot_reponse)
+VALUE('Arnaque au faux support technique.');
+
+INSERT INTO reponses(bot_reponse)
+VALUE('Arnaque au chantage à l’ordinateur ou webcam prétendus piratés.');
+
+INSERT INTO reponses(bot_reponse)
+VALUE('Escroquerie au Compte Personnel de Formation (CPF).');
+
+INSERT INTO reponses(bot_reponse)
+VALUE('Violation de données personnelles médicales de l’AP-HP.');
+
+INSERT INTO reponses(bot_reponse)
+VALUE('Faille de sécurité.');
+
+INSERT INTO reponses(bot_reponse)
+VALUE('Rançongiciel.');
+
+INSERT INTO reponses(bot_reponse)
+VALUE('Violation de données personnelles.');
+
+INSERT INTO reponses(bot_reponse)
+VALUE('Piratage de compte en ligne.');
+
+INSERT INTO reponses(bot_reponse)
+VALUE('Alerte de cybersécurité.');
+
+INSERT INTO reponses(bot_reponse)
+VALUE('Faille de sécurité.');
+
+INSERT INTO reponses(bot_reponse)
 VALUE('Quelles sont les mesures préventives?');
 
-INSERT INTO questions(bot_question)
-VALUE('L’escroquerie aux faux ordres de virement (FOVI)');
 
-INSERT INTO questions(bot_question)
-VALUE('Hameçonnage (phishing en anglais)');
 
-INSERT INTO questions(bot_question)
-VALUE('Arnaque au faux support technique');
+-- Partie cyber attaque 
 
-INSERT INTO questions(bot_question)
-VALUE('Arnaque au chantage à l’ordinateur ou webcam prétendus piratés');
-
--- Partie cyber attaque
-INSERT INTO reponses(bot_question)
+INSERT INTO reponses(bot_reponse)
 VALUE('j’ai subi une cyber attaque');
 
+INSERT INTO questions(bot_question)
+VALUE('Avez-vous besoin d’un diagnostique ou d’une assistance en ligne?');
 
+INSERT INTO reponses(bot_reponse)
+VALUE('Oui.');
+
+INSERT INTO reponses(bot_reponse)
+VALUE('Non.');
